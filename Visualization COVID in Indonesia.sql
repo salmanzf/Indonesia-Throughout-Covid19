c92304b8610ac8d3120af6efc9e4c01d447e01e5
Salman Zahid F <70428230+salmanzf@users.noreply.github.com>
2022-02-07 13:49:12 +0700

    Add files via upload

---
+++ Visualization COVID in Indonesia.sql
@@ -0,0 +1,39 @@
+--For References
+SELECT *
+FROM CovidDeaths;
+
+SELECT *
+FROM CovidDeaths;
+
+
+--For Visualization
+--TABLE
+SELECT SUM(population) population, SUM(total_cases) total_cases, (SUM(total_cases)/SUM(population))*100 total_cases_percentage
+FROM CovidDeaths
+WHERE continent IS NOT NULL;
+
+
+
+--TIME SERIES
+--Time Series Cases per Population Percentage
+SELECT location, date, population, total_cases, (total_cases/population)*100 Cases_Percentage
+FROM CovidDeaths
+ORDER BY location, date;
+--Time Series Death per Cases Percentage
+SELECT location, date, population, total_cases, total_deaths, (CAST(total_deaths AS int)/population)*100 Death_Percentage
+FROM CovidDeaths
+ORDER BY location, date;
+
+--BAR CHART
+--Bar Chart South East Asian Nation Total Vacc
+--Without Explicit LEFT JOIN function
+SELECT a.location, SUM(a.population) population, SUM(CAST(b.total_vaccinations AS bigint)) total_vaccination,
+		100*SUM(CAST(b.total_vaccinations AS bigint))/SUM(a.population) vaccination_rate
+FROM CovidDeaths a, CovidVaccinations b
+WHERE a.location = b.location
+AND a.date = b.date
+AND a.location IN ('Myanmar', 'Cambodia', 'Timor-Leste', 'Indonesia', 'Laos', 'Malaysia', 'Philippines',
+					'Singapore', 'Thailand', 'Vietnam')
+AND a.continent IS NOT NULL
+GROUP BY a.location
+
